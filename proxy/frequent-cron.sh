b878aa7c7e58ef5262e14c2c4807cf74a97ae208
ZekielDee <66716158+ZekielDee@users.noreply.github.com>
2021-09-08 20:59:16 -0700

    Delete proxy directory

--- proxy/frequent-cron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for ((i=1;i<=6;i++)); do
-    /bin/bash /update-haproxy.sh
-    sleep 10
-done
--- proxy/generate-haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /root/envs.sh
-
-cd /
-
-# Generate the HAProxy config
-/usr/bin/npm run generate-config /usr/local/etc/haproxy/haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg 
-echo "$(/bin/more /usr/local/etc/haproxy/haproxy.cfg)"
